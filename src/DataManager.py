353070f537a6bb6a361c61477f53f8c3489dae31
aegroto <aegroto@protonmail.com>
2020-09-21 09:31:25 +0200

    module-based data retrieve fixes

--- src/DataManager.py
+++ src/DataManager.py
@@ -1,8 +1,7 @@
 import yaml, os
 
 from modules.pytg.Manager import Manager
-
-from modules.pytg.ModulesLoader import ModulesLoader
+from modules.pytg.load import get_module_content_folder
 
 class DataManager(Manager):
     @staticmethod
@@ -19,33 +18,33 @@ class DataManager(Manager):
     # Database interface #
     ######################
 
-    def create_data(self, table, object_id, module="data"):
-        return self.save_data(table, object_id, self.load_data(table, "__default", module=module), module)
+    def create_data(self, module, table, object_id):
+        return self.save_data(module, table, object_id, self.load_data(module, table, "__default"))
 
-    def load_data(self, table, object_id, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def load_data(self, module, table, object_id):
+        module_folder = get_module_content_folder(module)
 
         return yaml.safe_load(open("{}/data/{}/{}.yaml".format(module_folder, table, object_id), "r"))
 
-    def save_data(self, table, object_id, data, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def save_data(self, module, table, object_id, data):
+        module_folder = get_module_content_folder(module)
 
         yaml.safe_dump(data, open("{}/data/{}/{}.yaml".format(module_folder, table, object_id), "w"))
 
         return data
 
-    def delete_data(self, table, object_id, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def delete_data(self, module, table, object_id):
+        module_folder = get_module_content_folder(module)
 
         os.remove("{}/data/{}/{}.yaml".format(module_folder, table, object_id))
 
-    def has_data(self, table, object_id, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def has_data(self, module, table, object_id):
+        module_folder = get_module_content_folder(module)
 
         return os.path.exists("{}/data/{}/{}.yaml".format(module_folder, table, object_id))
 
-    def load_table_entries(self, table, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def load_table_entries(self, module, table):
+        module_folder = get_module_content_folder(module)
 
         entries = []
 
@@ -55,14 +54,14 @@ class DataManager(Manager):
         for f in files:
             entry_id = f.replace(".yaml", "")
 
-            data = self.load_data(table, entry_id)
+            data = self.load_data(module, table, entry_id)
 
             entries.append(data)
 
         return entries
 
-    def find_entry_by_field(self, table, field, value, module="data"):
-        module_folder = ModulesLoader.get_module_content_folder(module)
+    def find_entry_by_field(self, module, table, field, value):
+        module_folder = get_module_content_folder(module)
 
         files = os.listdir("{}/data/{}".format(module_folder, table))
         files.remove("__default.yaml")
@@ -70,7 +69,7 @@ class DataManager(Manager):
         for f in files:
             entry_id = f.replace(".yaml", "")
 
-            data = self.load_data(table, entry_id)
+            data = self.load_data(module, table, entry_id)
 
             if data[field] == value:
                 return data
